bda6c6a548dadbeb772d658fa7e8ea47f4006598
aaron <aaron.lhh0401@gmail.com>
2021-05-27 17:11:18 -0700

    Slider object for project

--- LogoMaker/Slider.cpp
+++ LogoMaker/Slider.cpp
@@ -6,3 +6,119 @@
 //
 
 #include "Slider.hpp"
+#include "MouseEvents.hpp"
+
+Slider::Slider()
+:Slider("Slider", 500){
+    
+}
+
+
+Slider::Slider(string label)
+:Slider(label, 500){
+    
+}
+
+
+Slider::Slider(string label, float length){
+    // setup font
+    if(!States::loadFont){
+        if(!States::font.loadFromFile("OpenSans-Bold.ttf")){
+            cout << "Unable to find text file\n";
+        }
+        States::loadFont = true;
+    }
+    
+    // setup label and curVal
+    this->label.setFont(States::font);
+    this->label.setCharacterSize(States::TEXT_SIZE);
+    this->label.setFillColor(sf::Color::White);
+    this->label.setString(label+":  ");
+    this->curVal.setFont(States::font);
+    this->curVal.setCharacterSize(States::TEXT_SIZE);
+    this->curVal.setFillColor(sf::Color::White);
+    this->curVal.setString("0");
+    
+    // setup bar
+    this->slider.setSize({length, 3});
+    this->slider.setFillColor(sf::Color::White);
+    
+    // setup knob
+    this->knob.setRadius(15);
+    this->knob.setFillColor(sf::Color::White);
+    
+    // relative position of knob is initially zero
+    relPos = 0;
+    
+    // initialize position
+    setupPosition();
+}
+
+
+void Slider::setupPosition(){
+    float padding = 15;
+    
+    this->slider.setPosition(padding + label.getPosition().x+label.getGlobalBounds().width,
+                             label.getPosition().y+label.getCharacterSize()/4*3-slider.getGlobalBounds().height/2);
+    
+    this->knob.setPosition(slider.getPosition().x + relPos - knob.getRadius(),
+                           slider.getPosition().y+slider.getGlobalBounds().height/2-knob.getRadius());
+    
+    this->curVal.setPosition(slider.getPosition().x + slider.getGlobalBounds().width + padding + 10,
+                             label.getPosition().y+3);
+    
+}
+
+void Slider::setPosition(float x, float y){
+    this->label.setPosition(x, y);
+    setupPosition();
+}
+
+void Slider::setMax(float max){
+    this->slider.setSize({max, this->slider.getSize().y});
+    setupPosition();
+}
+
+void Slider::setLabel(string label){
+    this->label.setString(label+": ");
+    setupPosition();
+}
+
+int Slider::getCurVal(){
+    return relPos;
+}
+
+void Slider::addEventHandler(sf::RenderWindow& window, sf::Event event){
+    if(MouseEvents<sf::CircleShape>::draggedOver(knob, window, event)){
+        knob.setFillColor(sf::Color::Green);
+        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+        this->relPos = mousePos.x - slider.getPosition().x;
+        
+        // set boundary
+        if(relPos > slider.getSize().x)
+            relPos = slider.getSize().x;
+        else if(relPos < 0)
+            relPos = 0;
+        
+        // update curVal
+        this->curVal.setString(to_string(relPos));
+        
+        setupPosition();
+    }else{
+        knob.setFillColor(sf::Color::White);
+    }
+    
+}
+
+
+void Slider::update(){
+    // do nothing
+}
+
+
+void Slider::draw(sf::RenderTarget& window, sf::RenderStates states) const{
+    window.draw(label);
+    window.draw(slider);
+    window.draw(knob);
+    window.draw(curVal);
+}
--- LogoMaker/Slider.hpp
+++ LogoMaker/Slider.hpp
@@ -9,8 +9,9 @@
 #define Slider_hpp
 #include "States.hpp"
 
-class Slider{
+class Slider: public sf::Drawable, public sf::Transformable{
 public:
+    // constructors
     Slider();
     Slider(string label);
     Slider(string label, float length);
@@ -19,12 +20,28 @@ public:
     Slider(const Slider& other) = delete;
     Slider& operator=(const Slider& other) = delete;
     
+    // modification for slider
+    void setPosition(float x, float y);         // set position of slider
+    void setMax(float max);                 // set max number of slider
+    void setLabel(string label);            // set label of slider
+    
+    // accessors
+    int getCurVal();
+    
+    // GUI
+    virtual void draw(sf::RenderTarget& window, sf::RenderStates states) const;
+    void addEventHandler(sf::RenderWindow& window, sf::Event event);
+    void update();
     
     
 private:
-    sf::Text label;
+    sf::Text label;                 // label of slider
+    sf::Text curVal;                // current value of slider
     sf::RectangleShape slider;
     sf::CircleShape knob;
+    int relPos;                // relative position of knob in ref. of slider's starting pt
+    
+    void setupPosition();           // adjust position of curVal, slider and knob
 };
 
 #endif /* Slider_hpp */
