a347f36418f8ae37ae8d2d618626cf8f3e32ea3e
aaron <aaron.lhh0401@gmail.com>
2021-06-02 21:39:22 -0700

    Create FontSetting.hpp

---
+++ LogoMaker/FontSetting.hpp
@@ -0,0 +1,38 @@
+//
+//  FontSetting.hpp
+//  LogoMaker
+//
+//  Created by Aaron Lin on 6/2/21.
+//
+
+#ifndef FontSetting_hpp
+#define FontSetting_hpp
+#include "DropDownMenu.hpp"
+#include "GUIComponent.hpp"
+
+class FontSetting: public sf::Drawable, public sf::Transformable{
+public:
+    FontSetting();
+    
+    // setup pos
+    void setPosition(float x, float y);
+    
+    // accessors
+    States::fonts getFont();
+    States::fontStyle getStyle();
+    sf::FloatRect getGlobalBounds();
+    sf::Vector2f getPosition();
+    
+    // GUI
+    void draw(sf::RenderTarget& window, sf::RenderStates states) const;
+    void addEventHandler(sf::RenderWindow& window, sf::Event event);
+    void update();
+    
+    
+private:
+    sf::Text label;
+    DropDownMenu fontList;
+    DropDownMenu styleList;
+};
+
+#endif /* FontSetting_hpp */
